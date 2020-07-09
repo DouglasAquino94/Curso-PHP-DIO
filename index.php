d7cde40623604c11e9961a08ba717da530492ce9
Douglas Aquino <douglasaquino13@hotmail.com>
2020-07-08 22:16:28 -0300

    Criação do formulário

--- index.php
+++ index.php
@@ -1,50 +1,16 @@
-<?php
-    //Array vazio
-    $categorias = [];
-
-    //Adicionando itens ao Array
-    $categorias[] = 'infantil';
-    $categorias[] = 'adolescente';
-    $categorias[] = 'adulto';
-    $categorias[] = 'idoso';
-
-    //Array na tela
-    //print_r($categorias);
-
-    $nome = 'Eduardo';
-    $idade = '50';
-    
-
-    //var_dump($nome);
-    //var_dump($idade);
-
-    if($idade >= 6 && $idade < 12) {
-        for($c = 0; $c <= count($categorias); $c++){
-            if($categorias[$c] == 'infantil'){
-                echo 'Categoria: Infantil';
-            }
-        }
-
-    } else if ($idade >= 12 && $idade < 18){
-        for($c = 0; $c <= count($categorias); $c++){
-            if($categorias[$c] == 'adolescente'){
-                echo 'Categoria: Adolescente';
-            }
-        }
-
-    } else if ($idade >= 18 && $idade < 50) {
-        for($c = 0; $c <= count($categorias); $c++){
-            if($categorias[$c] == 'adulto'){
-                echo 'Categoria: Adulto';
-            }
-        }
-
-    } else {
-        for($c = 0; $c <= count($categorias); $c++){
-            if($categorias[$c] == 'idoso'){
-                echo 'Categoria: Idoso';
-            }
-        }
-    }
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso-PHP-DIO</title>
+</head>
+<body>
+    <h3>Dados pessoais</h3>
+    <form action="processaDados.php" method="post">
+        <p>Nome: <input type="text" name="nome" id="nome"></p>
+        <p>Idade: <input type="text" name="idade" id="idade"></p>
+        <p><input type="button" value="Enviar"></p>
+    </form>    
+</body>
+</html>
